d3c703c6d22e94716f0fdadce3b4f420be802921
Hiram Carreño <143430604+Abiff11@users.noreply.github.com>
2026-03-18 23:05:01 -0600

    Fix: Add allowed hosts for Render deployment

--- Front/project-PVE/vite.config.js
+++ Front/project-PVE/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['pve-frontend.onrender.com', 'pve-backend-latest.onrender.com'],
+  },
 })
